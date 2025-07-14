2d271ee304a3f21764d1ecf539293e2014ff71ac
dpqlsn <a01037111209@gmail.com>
2025-07-14 23:24:18 +0900

    fix :: 로그아웃 됨 알람 띄우기

--- src/components/Modal/Setting/SettingModal.tsx
+++ src/components/Modal/Setting/SettingModal.tsx
@@ -20,8 +20,8 @@ export default function SettingModal({ isOpen, onClose }: SettingModalProps) {
             <_.Title>로그아웃</_.Title>
             <_.Message>정말 로그아웃 하시겠습니까?</_.Message>
             <_.ButtonGroup>
-            <_.Button onClick={handleConfirm}>로그아웃</_.Button>
-            <_.Button className="cancel" onClick={onClose}>취소</_.Button>
+                <_.Button onClick={handleConfirm}>로그아웃</_.Button>
+                <_.Button className="cancel" onClick={onClose}>취소</_.Button>
             </_.ButtonGroup>
         </_.ModalContent>
         </_.Overlay>
